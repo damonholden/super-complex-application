5f8d05a46228b686dd6ca402926443f76ec46f59
damonholden <damonh003@gmail.com>
2022-11-05 14:46:51 +0000

    build: add jest eslint support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react'],
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', 'jest'],
 };
