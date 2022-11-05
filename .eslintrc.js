204a1552650739b3644f6b7abce995421b2b5964
damonholden <damonh003@gmail.com>
2022-11-05 14:34:46 +0000

    build: add prettier to dev-dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,17 +4,11 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
   plugins: ['react'],
-  rules: {
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'windows'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-  },
 };
