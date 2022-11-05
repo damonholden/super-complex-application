c6fca9de725565426d79e95438377949418898da
damonholden <damonh003@gmail.com>
2022-11-05 14:55:01 +0000

    fix: import jest variables to test file

--- tests/unit/screamString.test.js
+++ tests/unit/screamString.test.js
@@ -1,20 +1,21 @@
-import { screamString } from "../../frontend/src/util/screamString"
+import { describe, expect, test } from '@jest/globals';
+import { screamString } from '../../frontend/src/util/screamString';
 
 describe('screamString', () => {
-  test('should return an uppercase string with an exclamation point', () => {
-    expect(screamString('hello')).toBe('HELLO!');
-  });
+	test('should return an uppercase string with an exclamation point', () => {
+		expect(screamString('hello')).toBe('HELLO!');
+	});
 
-  test('should return an error message if an empty string is provided', () => {
-    expect(screamString('')).toBe('please provide a string');
-  });
+	test('should return an error message if an empty string is provided', () => {
+		expect(screamString('')).toBe('please provide a string');
+	});
 
-  test('should return an error message if it receives anything other than a single string', () => {
-    expect(screamString(123)).toBe('please provide a string');
-    expect(screamString({})).toBe('please provide a string');
-    expect(screamString([])).toBe('please provide a string');
-    expect(screamString(true)).toBe('please provide a string');
-    expect(screamString(null)).toBe('please provide a string');
-    expect(screamString(undefined)).toBe('please provide a string');
-  });
+	test('should return an error message if it receives anything other than a single string', () => {
+		expect(screamString(123)).toBe('please provide a string');
+		expect(screamString({})).toBe('please provide a string');
+		expect(screamString([])).toBe('please provide a string');
+		expect(screamString(true)).toBe('please provide a string');
+		expect(screamString(null)).toBe('please provide a string');
+		expect(screamString(undefined)).toBe('please provide a string');
+	});
 });
